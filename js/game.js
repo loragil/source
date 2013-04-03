671f9abfbc8468432a0893287acba0190b339f54
Gil Alexander <loragil@gmail.com>
2013-04-04 01:30:10 +0300

    monopoly

--- js/game.js
+++ js/game.js
@@ -1,4 +1,10 @@
-(function(){
-	var tg = tg || {};
-	//TODO: sasdads	
-})();
+var mg = mg || {};//namespace declaration ('mg' stands for 'Monopoly Game').
+(function () {
+    "use strict";
+
+    mg.game = {
+        init: function () {}
+    };
+}());
+
+
